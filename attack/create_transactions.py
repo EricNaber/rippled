553ff6aec0eb8549b608b19462cb7d17aee7aebf
EricNaber <naber2504@gmail.com>
2023-10-16 20:02:21 +0200

    change transactions in attack-folder

--- attack/create_transactions.py
+++ attack/create_transactions.py
@@ -1,5 +1,12 @@
 from xrpl.wallet import Wallet
+from xrpl.models.transactions import Payment
+from xrpl.utils import xrp_to_drops
+from xrpl.transaction import sign
 
+
+# rippled submit snoPBrXtMeMyMHUVTgbuqAfg1SUTb '{"Account":"rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh","Amount": "1000000000","Destination":"rfhWbXmBpxqjUWfqVv34t4pHJHs6YDFKCN","TransactionType": "Payment","Fee": "10" }'
+# rippled submit sEd7gsxCwikqZ9C81bjKMFNM9xoReYU '{"Account":"rfhWbXmBpxqjUWfqVv34t4pHJHs6YDFKCN","Amount": "1000000000","Destination":"rG1eMisac1neCXeZNPYmwV8sovo5vs9dnB","TransactionType": "Payment","Fee": "10" }'
+# rippled submit sEd7gsxCwikqZ9C81bjKMFNM9xoReYU '{"Account":"rfhWbXmBpxqjUWfqVv34t4pHJHs6YDFKCN","Amount": "1000000000","Destination":"rnkP5Tipm14sqpoDetQxrLjiyyKhk72eAi","TransactionType": "Payment","Fee": "10" }'
 
 def main():
     genesis_seed = "snoPBrXtMeMyMHUVTgbuqAfg1SUTb"
@@ -12,18 +19,40 @@ def main():
     dest2_wallet = Wallet.from_seed(seed=dest2_seed)
 
     # rippled works unexpected with " or '. Encoding transactions as one string, however, is enough for our purpose.
-    base_tx = "{ \"Account\": \"rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh\", \"Amount\": \"1000000000\", \"Destination\": \"$(src_address)\", \
-               \"TransactionType\": \"Payment\", \"Fee\": \"10\" }".replace("$(src_address)", src_wallet.address)   # from genesis to src_address
-    tx1 = "{ \"Account\": \"$(src_address)\", \"Amount\": \"1000000000\", \
-           \"Destination\": \"$(dest_address)\", \"TransactionType\": \"Payment\", \
-           \"Fee\": \"10\" }".replace("$(src_address)", src_wallet.address).replace("$(dest_address)", dest1_wallet.address)    # from src_address to dest1_address
-    tx2 = "{ \"Account\": \"$(src_address)\", \"Amount\": \"1000000000\", \
-           \"Destination\": \"$(dest_address)\", \"TransactionType\": \"Payment\", \
-           \"Fee\": \"10\" }".replace("$(src_address)", src_wallet.address).replace("$(dest_address)", dest2_wallet.address)    # from src_address to dest2_address
+    # base_tx = "{ \"Account\": \"rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh\", \"Amount\": \"1000000000\", \"Destination\": \"$(src_address)\", \
+    #            \"TransactionType\": \"Payment\", \"Fee\": \"10\" }".replace("$(src_address)", src_wallet.address)   # from genesis to src_address
+    # tx1 = "{ \"Account\": \"$(src_address)\", \"Amount\": \"1000000000\", \
+    #        \"Destination\": \"$(dest_address)\", \"TransactionType\": \"Payment\", \
+    #        \"Fee\": \"10\" }".replace("$(src_address)", src_wallet.address).replace("$(dest_address)", dest1_wallet.address)    # from src_address to dest1_address
+    # tx2 = "{ \"Account\": \"$(src_address)\", \"Amount\": \"1000000000\", \
+    #        \"Destination\": \"$(dest_address)\", \"TransactionType\": \"Payment\", \
+    #        \"Fee\": \"10\" }".replace("$(src_address)", src_wallet.address).replace("$(dest_address)", dest2_wallet.address)    # from src_address to dest2_address
 
-    print(f"rippled submit {genesis_seed} '{base_tx}'")
-    print(f"rippled submit {src_seed} '{tx1}'")
-    print(f"rippled submit {src_seed} '{tx2}'")
+    tx1 = Payment(
+        account=src_wallet.address,
+        amount=xrp_to_drops(22),
+        destination=dest1_wallet.address,
+        last_ledger_sequence= 500
+    )
+    tx1_signed = sign(tx1, src_wallet)
+
+    tx2 = Payment(
+        account=src_wallet.address,
+        amount=xrp_to_drops(10),
+        destination=dest2_wallet.address, 
+        sequence=30
+    )
+    tx2_signed = sign(tx2, src_wallet)
+
+    print(f"tx1 blob: {tx1_signed.blob()}")
+    # print(f"tx2 blob: {tx2_signed.blob()}")
+
+    
+
+
+    # print(f"rippled submit {genesis_seed} '{base_tx}'")
+    # print(f"rippled submit {src_seed} '{tx1}'")
+    # print(f"rippled submit {src_seed} '{tx2}'")
 
 
 if __name__=="__main__":
